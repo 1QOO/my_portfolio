ebec32022a16c4c88e2fe58556ca0d661cfa0803
1QOO <iqro.s.ahfadz@gmail.com>
2025-10-02 19:28:51 +0700

    Installed framer-motion

--- src/layouts/projects-display.jsx
+++ src/layouts/projects-display.jsx
@@ -6,7 +6,7 @@ const ProjectsDisplay = ()=>{
         <section className="w-full">
             <h2 className="bg-gray-800 p-4 text-[1.5rem] text-white">My Projects</h2>
             <div className='flex flex-col flex-wrap p-4 gap-[2rem] justify-center items-center'>
-                {projects.map(project=> <Project project={project} />)}
+                {projects.map(project=> <Project key={project.title} project={project} />)}
             </div>
         </section>
     )
--- src/ui/project.jsx
+++ src/ui/project.jsx
@@ -1,7 +1,13 @@
 import { LinksProject } from "./links-project"
+import { motion } from 'framer-motion'
+
 export default function Project({project}){
     return (
-        <div className="flex flex-col md:lg:flex-row flex-nowrap gap-[1.5rem] w-full grow p-[1rem] shadow-[0_0_5px_1px_gray] rounded-[20px]">
+        <motion.div className="flex flex-col md:lg:flex-row flex-nowrap gap-[1.5rem] w-full grow p-[1rem] shadow-[0_0_5px_1px_gray] rounded-[20px]"
+        initial={{opacity:0 , x:"100%"}}
+        whileInView={{opacity:1 , x:0}}
+        transition={{duration: 1}}
+        viewport={{once: true}}>
             <img className="max-h-[300px] max-w-[300px] object-contain shrink-1 grow-0" src={project.img} alt={project.title} />
             <div className="grow">
                 <h3 className="text-2xl">{project.title}</h3>
@@ -10,18 +16,18 @@ export default function Project({project}){
                 </p>
                 <h4 className="font-bold mt-2">Tech Stack</h4>
                 <ul>
-                    {project.techStack.map(item=><li>{item}</li>)}
+                    {project.techStack.map((item,i)=><li key={i}>{item}</li>)}
                 </ul>
                 {project.features && 
                 <>
                 <h4 className="font-bold mt-2">Features</h4>
                 <ul>
-                    {project.features.map(item=><li>{item}</li>)}
+                    {project.features.map((item,i)=><li key={i}>{item}</li>)}
                 </ul>
                 </>
                 }
                 <LinksProject project={project} />
             </div>
-        </div>
+        </motion.div>
     )
 }
